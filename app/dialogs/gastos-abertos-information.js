53bd1ac7bd9a48da745f46aa10beaa003df0ee74
Lucas Ansei <lucas.ansei@eokoe.com>
2017-08-16 16:38:51 -0300

    Adding line breaks

--- app/dialogs/gastos-abertos-information.js
+++ app/dialogs/gastos-abertos-information.js
@@ -16,8 +16,8 @@ const no                             = "Não";
 library.dialog('/', [
     (session) => {
         builder.Prompts.choice(session,
-            "Que bom, a equipe do Gastos Abertos tem o objetivo de conectar cidadãos com o orçamento público.\
-            Acreditamos na mobilização e na educação cidadã sobre transparência nos municípios brasileiros.\
+            "Que bom, a equipe do Gastos Abertos tem o objetivo de conectar cidadãos com o orçamento público.<br><br />\
+            Acreditamos na mobilização e na educação cidadã sobre transparência nos municípios brasileiros.<br><br />\
             Quer conhecer mais sobre:",
             [ gastosAbertosCicles, secondGastosAbertosCicle, gameSignUp, firstGastosAbertosCicleResults ],
             { listStyle: builder.ListStyle.button }
