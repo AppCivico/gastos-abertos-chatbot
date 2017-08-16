03295a35d8f8672bdd722a13f0e3ce781373d5e0
Lucas Ansei <lucas.ansei@eokoe.com>
2017-08-16 14:57:29 -0300

    Changing env to envfile

--- app/app.js
+++ app/app.js
@@ -17,37 +17,15 @@ bot.dialog('/getstarted', [
         console.log(session.userData);
         session.sendTyping();
         if( !session.userData.firstRun ) {
-            // Store the returned user page-scoped id (USER_ID) and page id
+
             session.userData.userid = session.message.sourceEvent.sender.id;
             session.userData.pageid = session.message.sourceEvent.recipient.id;
 
-            // DELAY ISN'T THE REQUEST - I THINK IT'S THE INITIAL REQUEST TO BOTFRAMEWORK
-
-            // Move to the /getprofile dialog
             session.beginDialog('/promptButtons');
         } else {
-            // The firstname has been stored so the user has completed the /getstarted dialog
-            // Stop this dialog and Welcome them back
             session.replaceDialog('/promptButtons');
         }
     }
-    /*(session) => {
-        if (session.message.address.channelId == "facebook" && session.message.text == "GET_STARTED") {
-            session.send({
-                attachments: [
-                    {
-                        contentType: 'image/jpeg',
-                        contentUrl: "https://gallery.mailchimp.com/cdabeff22c56cd4bd6072bf29/images/8e84d7d3-bba7-43be-acac-733dd6712f78.png"
-                    }
-                ]
-            });
-            session.replaceDialog('/promptButtons');
-        }
-
-        if (session.message.address.channelId == "emulator") {
-            session.replaceDialog('/promptButtons');
-        }
-    }*/
 ]);
 
 bot.dialog('/promptButtons', [
