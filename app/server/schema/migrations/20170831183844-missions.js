e339ef2575af24e88473d77d0d8df160778d333a
lucas <lucas@lucastakushi.com>
2017-09-02 22:45:09 -0300

    Populating the missions table

--- app/server/schema/migrations/20170831183844-missions.js
+++ app/server/schema/migrations/20170831183844-missions.js
@@ -2,22 +2,33 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    queryInterface.bulkInsert('missions', [
+      {
+        code: 1,
+        name: 'Diagnóstico do portal de transparência',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        code: 2,
+        name: 'Assinatura da carta compromisso',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        code: 3,
+        name: 'Pedido de acesso à informação',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    queryInterface.bulkInsert('missions', [
+      { code: 1 },
+      { code: 2 },
+      { code: 3 },
+    ]);
   }
 };
