a6b8a2b87e1e2241d5be1c8137d57bbfb831109e
Lucas Ansei <lucas.ansei@eokoe.com>
2017-10-02 10:53:40 -0300

    Changing the extra task to the first mission conclusion

--- app/dialogs/first_mission/assign.js
+++ app/dialogs/first_mission/assign.js
@@ -69,26 +69,7 @@ library.dialog('/', [
         });
     },
     (session, args) => {
-        msg = new builder.Message(session);
-        msg.sourceEvent({
-            facebook: {
-                attachment:{
-                  type:"template",
-                  payload:{
-                    template_type:"generic",
-                    elements:[{
-                        title:"Olá! Eu sou o Guaxi!",
-                        subtitle:"O chatbot mais transparente e engajado da internet! Venha conversar comigo!",
-                        image_url:"https://gallery.mailchimp.com/cdabeff22c56cd4bd6072bf29/images/8e84d7d3-bba7-43be-acac-733dd6712f78.png",
-                        item_url: "http://m.me/gastosabertos",
-                        buttons:[{
-                            type:"element_share"
-                          }]
-                        }]
-                    }
-                }
-            }
-        });
+        
 
         switch(args.response.entity) {
             case Yes:
@@ -121,43 +102,14 @@ library.dialog('/', [
                 break;
         }
 
-
-        User.count({
-                where: {
-                    state: user.state
-                }
-            })
-            .then(count => {
-                if (count < 10 && count != 1) {
-                    session.send("E eu vou te dar uma tarefa extra " + emoji.grinningface + emoji.sunglass + "\n\nAtualmente há " + count + " líderes no seu estado. Vamos aumentar este número para 10 líderes?");
-                    session.send("Para alcançar esse número pedimos que você convide seus amigos para participar desse nosso segundo ciclo do Gastos Abertos!");
-                    if (session.message.address.channelId == 'facebook') {
-                        session.send(msg);
-                    }
-                }
-                else if (count < 10 && count == 1) {
-                    session.send("E eu vou te dar uma tarefa extra " + emoji.grinningface + emoji.sunglass + "\n\nAtualmente há apenas você de líder no seu estado. Vamos aumentar este número para 10 líderes?");
-                    session.send("Compartilhe isto com os seus amigos! Assim nós teremos mais força para incentivar a transparência em seu estado!");
-                    if (session.message.address.channelId == 'facebook') {
-                        session.send(msg);
-                    }
-                }
-
-                builder.Prompts.choice(session,
-                    'Posso te ajudar com mais alguma coisa?',
-                    [MoreInformations, Conclusion, Contact, Restart],
-                    {
-                        listStyle: builder.ListStyle.button,
-                        retryPrompt: retryPrompts.choice
-                    }
-                );
-            })
-            .catch(e => {
-                console.log("Error" + e);
-                session.send("Oooops, tive um problema ao iniciar suas missões, tente novamente mais tarde e entre em contato conosco.");
-                session.endDialogWithResult({ resumed: builder.ResumeReason.notCompleted });
-                throw e;
-            });
+        builder.Prompts.choice(session,
+            'Posso te ajudar com mais alguma coisa?',
+            [MoreInformations, Conclusion, Contact, Restart],
+            {
+                listStyle: builder.ListStyle.button,
+                retryPrompt: retryPrompts.choice
+            }
+        );
     },
 
     (session, args) => {
--- app/dialogs/first_mission/conclusion.js
+++ app/dialogs/first_mission/conclusion.js
@@ -22,6 +22,7 @@ UserMission = require('../../server/schema/models').user_mission;
 const Yes              = "Sim";
 const No               = "Não";
 const MoreInformations = "Detalhes da missão";
+const Confirm          = "Beleza!";
 
 let user;
 let user_mission;
@@ -335,6 +336,57 @@ library.dialog('/transparencyPortalHasBiddingProcessData', [
 
 library.dialog('/userUpdate', [
     (session) => {
+        msg = new builder.Message(session);
+        msg.sourceEvent({
+            facebook: {
+                attachment:{
+                  type:"template",
+                  payload:{
+                    template_type:"generic",
+                    elements:[{
+                        title:"Olá! Eu sou o Guaxi!",
+                        subtitle:"O chatbot mais transparente e engajado da internet! Venha conversar comigo!",
+                        image_url:"https://gallery.mailchimp.com/cdabeff22c56cd4bd6072bf29/images/8e84d7d3-bba7-43be-acac-733dd6712f78.png",
+                        item_url: "http://m.me/gastosabertos",
+                        buttons:[{
+                            type:"element_share"
+                          }]
+                        }]
+                    }
+                }
+            }
+        });
+
+        session.send("Uhuuu! Concluímos nossa primeira missão!\n\nEu disse que formariamos uma boa equipe!" + emoji.sunglass + emoji.clap);
+
+        User.count({
+                where: {
+                    state: user.state
+                }
+            })
+            .then(count => {
+                if (count < 10 && count != 1) {
+                    session.send("E eu vou te dar uma tarefa extra " + emoji.grinningface + emoji.sunglass + "\n\nAtualmente há " + count + " líderes no seu estado. Vamos aumentar este número para 10 líderes?");
+                    session.send("Para alcançar esse número pedimos que você convide seus amigos para participar desse nosso segundo ciclo do Gastos Abertos!");
+                    if (session.message.address.channelId == 'facebook') {
+                        session.send(msg);
+                    }
+                }
+                else if (count < 10 && count == 1) {
+                    session.send("E eu vou te dar uma tarefa extra " + emoji.grinningface + emoji.sunglass + "\n\nAtualmente há apenas você de líder no seu estado. Vamos aumentar este número para 10 líderes?");
+                    session.send("Compartilhe isto com os seus amigos! Assim nós teremos mais força para incentivar a transparência em seu estado!");
+                    if (session.message.address.channelId == 'facebook') {
+                        session.send(msg);
+                    }
+                }
+            })
+            .catch(e => {
+                console.log("Error" + e);
+                session.send("Oooops, tive um problema ao iniciar suas missões, tente novamente mais tarde e entre em contato conosco.");
+                session.endDialogWithResult({ resumed: builder.ResumeReason.notCompleted });
+                throw e;
+        });
+
         UserMission.update({
             completed: true,
             metadata: answers
@@ -348,10 +400,14 @@ library.dialog('/userUpdate', [
         })
         .then(result => {
             console.log(result + "Mission updated sucessfuly");
-            session.send("Uhuuu! Concluímos nossa primeira missão!\n\nEu disse que formariamos uma boa equipe!" + emoji.sunglass + emoji.clap);
-            session.send("Agora pode ficar tranquilo que eu irei te chamar quando a gente puder começar a segunda missão, okay?" + emoji.smile);
-            session.endDialogWithResult({ resumed: builder.ResumeReason.completed });
-            session.beginDialog('/welcomeBack');
+            builder.Prompts.choice(session,
+                "Agora pode ficar tranquilo que eu irei te chamar quando a gente puder começar a segunda missão, okay?" + emoji.smile,
+                [ Confirm ],
+                {
+                    listStyle: builder.ListStyle.button,
+                    retryPrompt: retryPrompts.choice
+                }
+            );
         })
         .catch(e => {
             console.log("Error updating mission" + e);
@@ -360,6 +416,15 @@ library.dialog('/userUpdate', [
             throw e;
         });
     },
+
+    (session, args) => {
+        switch (args.response.entity) {
+            case Confirm:
+                session.endDialogWithResult({ resumed: builder.ResumeReason.completed });
+                session.beginDialog('/welcomeBack');
+                break;
+        }
+    }
 ]).cancelAction('cancelar', null, { matches: /^cancelar/i });
 
 module.exports = library;
