0505491d6cf8a3f9ffea75a61d7508575e7614f5
Jordan Victor <jordanvscher@hotmail.com>
2018-01-19 11:23:29 -0200

    Text changes

--- app/validators.js
+++ app/validators.js
@@ -4,7 +4,7 @@
 
 const library = new builder.Library('validators');
 
-const PhoneRegex = new RegExp(/^(\(?[0-9]{2}\)?)(\s)?([9]{1})?([0-9]{4})-?([0-9]{4})$/);
+const PhoneRegex = new RegExp(/^(\(?[0-9]{2}\)?)[\s-]?([9]{1})?([0-9]{4})-?([0-9]{4})$/);
 const StateRegex = new RegExp(/^((AC)|(AL)|(AP)|(AM)|(BA)|(CE)|(DF)|(ES)|(GO)|(MA)|(MT)|(MS)|(MG)|(PA)|(PB)|(PR)|(PE)|(PI)|(RJ)|(RN)|(RS)|(RO)|(RR)|(SC)|(SP)|(SE)|(TO)|(ac)|(al)|(ap)|(am)|(ba)|(ce)|(df)|(es)|(go)|(ma)|(mt)|(ms)|(mg)|(pa)|(pb)|(pr)|(pe)|(pi)|(rj)|(rn)|(rs)|(ro)|(rr)|(sc)|(sp)|(se)|(to))$/);
 const DateRegex = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/); // eslint-disable-line no-useless-escape
 const MailRegex = new RegExp(/^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-?\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/); // eslint-disable-line no-useless-escape
