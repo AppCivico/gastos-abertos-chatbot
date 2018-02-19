e3ea9a5f30a5da1d16de34e0cc4e8bddeb8758a4
Jordan Victor <jordanvscher@hotmail.com>
2018-02-19 12:51:55 -0300

    Halfway => message options

--- app/dialogs/gastos-abertos-information.js
+++ app/dialogs/gastos-abertos-information.js
@@ -11,14 +11,17 @@ const library = new builder.Library('gastosAbertosInformation');
 const accessLaw = 'Dados abertos?';
 const contact = 'Entrar em contato';
 const reset = 'Voltar ao início';
-// const signEmail = 'Cadastrar e-mail';
-// const changeEmail = 'Trocar e-mail';
-// const keepEmail = 'Manter o mesmo';
-// let User;
+const receiveMessage = 'Receber Mensagem';
+let receiveDialog;
+let receiveYes;
+let receiveNo;
+let newAddress;
+
+let User;
 
 library.dialog('/', [
-	(session) => {
-		// [User] = [args.User];
+	(session, args) => {
+		[User] = [args.User];
 		session.sendTyping();
 		session.send('A equipe Gastos Abertos tem o objetivo de conectar cidadãos com o orçamento público.' +
 		'\n\nAcreditamos na mobilização e na educação cidadã sobre transparência nos municípios brasileiros.');
@@ -32,7 +35,7 @@ library.dialog('/promptButtons', [
 		builder.Prompts.choice(
 			session,
 			`Como posso te ajudar? ${emoji.get('slightly_smiling_face').repeat(2)}`,
-			[accessLaw, contact, reset],
+			[accessLaw, receiveMessage, contact, reset],
 			{
 				listStyle: builder.ListStyle.button,
 				retryPrompt: retryPrompts.about,
@@ -46,6 +49,9 @@ library.dialog('/promptButtons', [
 			switch (result.response.entity) {
 			case accessLaw:
 				session.beginDialog('/accessLaw');
+				break;
+			case receiveMessage:
+				session.beginDialog('/receiveMessage');
 				break;
 			case contact:
 				session.beginDialog('contact:/');
@@ -78,96 +84,87 @@ library.dialog('/accessLaw', [
 		session.replaceDialog('/promptButtons');
 	},
 ]);
-/*
-library.dialog('/signEmail', [
+
+library.dialog('/receiveMessage', [
 	(session) => {
-		session.send('Aqui você poderá vincular o seu e-mail ao projeto! ' +
-		`${emoji.get('slightly_smiling_face').repeat(2)}`);
 		User.findOne({
 			where: { fb_id: session.userData.userid },
 		}).then((user) => {
-			if (user.get('email') === 'undefined') { // in case there's no e-mail
-				session.replaceDialog('/askEmail');
+			if (user.get('address') === null) {
+				receiveDialog = 'Você não está recebendo nenhum de nossas mensagens diretas.\n\nDeseja começar a recebê-las?';
+				receiveYes = 'Sim, quero receber!';
+				receiveNo = 'Não quero receber';
+				newAddress = session.message.address;
 			} else {
-				session.send(`O e-mail '${user.get('email')}' já está cadastrado`);
-				session.replaceDialog('/changeEmail');
+				receiveDialog = 'Você já recebe nossas mensagens diretas. Deseja parar de recebê-las?';
+				receiveYes = 'Parar de receber';
+				receiveNo = 'Continuar recebendo';
+				newAddress = null;
 			}
+			session.replaceDialog('/updateAddress');
 		}).catch((err) => {
 			console.log(err);
-			session.send(`Desculpe-me. ${emoji.get('dizzy_face').repeat(2)}.`);
+			session.send(`Desculpe-me. ${emoji.get('dizzy_face').repeat(2)}. Estou com problemas técnicos no momento.`);
 			session.replaceDialog('/promptButtons');
 		});
 	},
 ]);
 
-library.dialog('/askEmail', [
-	(session) => {
-		session.beginDialog('validators:email', {
-			prompt: `Digite seu e-mail ou entre com 'cancelar' para voltar ${emoji.get('email')}`,
-			retryPrompt: retryPrompts.email,
-			maxRetries: 5,
-		});
-	},
-	(session, args) => {
-		if (args.resumed) {
-			session.send(`Você tentou inserir um e-mail inválido muitas vezes.` +
-			' ${emoji.get('dizzy_face').repeat(2)} Tente novamente mais tarde.');
-		} else if (args.response === true) {
-			session.replaceDialog('/promptButtons');
-		} else {
-			User.findOne({
-				where: { fb_id: session.userData.userid },
-			}).then((user) => {
-				user.updateAttributes({
-					email: args.response,
-				});
-				session.send(`E-mail cadastrado com sucesso! ${emoji.get('sunglasses').repeat(2)}`);
-				session.replaceDialog('/promptButtons');
-			}).catch((err) => {
-				console.log(err);
-				session.send(`Desculpe-me. ${emoji.get('dizzy_face').repeat(2)}`);
-			});
-		}
-	},
-]).customAction({
-	matches: /^cancel$|^cancelar$|^voltar$|^in[íi]cio$|^começar/i,
-	onSelectAction: (session) => {
-		session.endDialog();
-	},
-});
-
-library.dialog('/changeEmail', [
+library.dialog('/updateAddress', [
 	(session) => {
 		builder.Prompts.choice(
-			session,
-			'Você pode trocar o e-mail cadastrado por outro ou manter o mesmo.',
-			[changeEmail, keepEmail],
+			session, receiveDialog,	[receiveYes, receiveNo],
 			{
 				listStyle: builder.ListStyle.button,
-				retryPrompt: retryPrompts.choiceIntent,
-				promptAfterAction: false,
+				retryPrompt: retryPrompts.about,
 			} // eslint-disable-line comma-dangle
 		);
 	},
+
 	(session, result) => {
-		session.sendTyping();
 		if (result.response) {
 			switch (result.response.entity) {
-			case changeEmail:
-				session.replaceDialog('/askEmail');
+			case receiveYes:
+				session.send('Ótimo! Espero que você nos ajude na divulgação de conteúdo e informações sobre ' +
+				'dados abertos e transparência orçamentária na sua cidade ou em seu círculo de amizades!');
+				User.update({
+					address: newAddress,
+				}, {
+					where: {
+						fb_id: session.userData.userid,
+					},
+					returning: true,
+				})
+					.then(() => {
+						console.log('User address updated sucessfuly');
+					})
+					.catch((err) => {
+						console.log(err);
+						throw err;
+					});
 				break;
-			default: // keepEmail
-				session.replaceDialog('/promptButtons');
+			default: // receiveNo
+				session.send(`Tranquilo! Você poderá se inscrever no menu de informações. ${emoji.get('smile')}`);
+				// User.update({
+				// 	address: null,
+				// }, {
+				// 	where: {
+				// 		fb_id: session.userData.userid,
+				// 	},
+				// 	returning: true,
+				// })
+				// 	.then(() => {
+				// 		console.log('User address erased sucessfuly');
+				// 	})
+				// 	.catch((err) => {
+				// 		console.log(err);
+				// 		throw err;
+				// 	});
 				break;
 			}
+			session.replaceDialog('/promptButtons');
 		}
 	},
-]).customAction({
-	matches: /^cancel$|^cancelar$|^voltar$|^in[íi]cio$|^começar/i,
-	onSelectAction: (session) => {
-		session.endDialog();
-	},
-});
-*/
+]);
 
 module.exports = library;
