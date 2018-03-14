35f8987ef270086cff4d7680e92debab51f9b847
Jordan Victor <jordanvscher@hotmail.com>
2018-03-14 18:29:16 -0300

    More time wasted...

--- app/panel/get-users.js
+++ app/panel/get-users.js
@@ -1,18 +1,21 @@
 /* global builder:true */
-// Check how many users are in a state
+// Sends a download link to a csv with useful inforation
 
 const library = new builder.Library('csvUser');
 
 const fs = require('fs');
 const request = require('request');
 const csvWriter = require('csv-write-stream');
-
+const csvReader = require('fast-csv');
 const Base64File = require('js-base64-file');
 
-const generatedRequest = new Base64File();
-const path = '/tmp/';
+const usersFile = new Base64File();
+
+const path = `${__dirname}/`;
 let file = '';
 let writer;
+let csvData = [];
+let dataString;
 
 const apiUri = process.env.MAILCHIMP_API_URI;
 const apiUser = process.env.MAILCHIMP_API_USER;
@@ -27,10 +30,10 @@ const User = require('../server/schema/models').user;
 library.dialog('/', [
 	(session, args, next) => {
 		writer = csvWriter();
-		file = 'guaxi_usuario2.csv';
+		file = `${Math.floor(Date.now() / 1000)}_user_guaxi.csv`;
 		User.findAndCountAll({
-			attributes: ['fb_name', 'name', 'state', 'city', 'receiveMessage', 'group'],
-			order: [['createdAt', 'DESC']], // order by last recorded interation with bot
+			attributes: ['id', 'fb_name', 'name', 'state', 'city', 'receiveMessage', 'group', 'createdAt', 'updatedAt'],
+			order: [['id', 'ASC']], // order by last recorded interation with bot
 			// admin: {
 			// 	$eq: false, // we're not counting admins as users
 			// },
@@ -40,19 +43,21 @@ library.dialog('/', [
 				session.endDialog();
 			} else {
 				let count = 0;
-				writer.pipe(fs.createWriteStream(file));
+				writer.pipe(fs.createWriteStream(path + file));
 				session.send(`Encontrei ${listUser.count} usuário(s).`);
 				listUser.rows.forEach((element) => {
 					writer.write({
 						Número: ++count, // eslint-disable-line no-plusplus
+						ID: element.dataValues.id,
 						'Nome no Facebook': element.dataValues.fb_name,
 						'Nome cadastrado': element.dataValues.name,
 						Estado: element.dataValues.state,
 						Município: element.dataValues.city,
 						'Recebe Mensagem': element.dataValues.receiveMessage,
 						Grupo: element.dataValues.group,
+						'Criado em': element.dataValues.createdAt,
+						'Última Interação': element.dataValues.updatedAt,
 					});
-
 					// this block will be executed last
 					if (count === listUser.rows.length) {
 						writer.end();
@@ -66,9 +71,28 @@ library.dialog('/', [
 		});
 	},
 	(session, args, next) => {
-		let data = generatedRequest.loadSync('', file);
-		data = JSON.stringify(data);
-		const dataString = `{"name":"${Math.floor(Date.now() / 1000)}_user_guaxi.csv" , "file_data":${data}}`;
+		fs.createReadStream(path + file)
+			.pipe(csvReader())
+			.on('data', (data) => {
+				console.log(data);
+				csvData.push(data);
+			})
+			.on('end', () => {
+				console.log(`Finished reading csv data => ${file}`);
+				csvData = Buffer.from(csvData).toString('base64');
+				csvData = JSON.stringify(csvData);
+				next();
+			});
+	},
+	(session, args, next) => {
+		// console.log(`csvData:${csvData}`);
+		// csvData = usersFile.loadSync(path, file);
+		// console.log(`csvData2:${csvData}`);
+		// csvData = JSON.stringify(csvData);
+		// console.log(`csvData3:${csvData}`);
+
+		dataString = `{"name":${file} , "file_data":${csvData}}`;
+		console.dir(dataString);
 
 		const options = {
 			url: apiUri,
@@ -84,6 +108,7 @@ library.dialog('/', [
 		function callback(error, response, body) {
 			if (!error || response.statusCode === 200) {
 				const obj = JSON.parse(body);
+				console.log(obj);
 				console.log(obj.full_size_url);
 				const msg = new builder.Message(session);
 				msg.sourceEvent({
@@ -108,10 +133,20 @@ library.dialog('/', [
 				});
 				session.send(msg);
 				next();
+			} else {
+				console.log(error);
+				console.log(response.statusCode);
+				console.log(response.statusMessage);
+				next();
 			}
 		}
+		console.log('Executando request...');
 		request(options, callback);
-		// fs.unlink(file);
+	},
+	(session) => {
+		fs.unlink(path + file, () => {
+			session.endDialog();
+		});
 	},
 ]);
 
